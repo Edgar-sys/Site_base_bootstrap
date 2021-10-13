3964b61b6d05c8706b3bc3a5d48a0756c963b63a
Edgar-sys <80836058+Edgar-sys@users.noreply.github.com>
2021-10-13 11:14:13 -0300

    mudei tudo

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,31 @@
+function mostra_resumo(){
+    const item1 = document.getElementById
+    ("resumo");
+    item1.style.display = "block";
+}
+
+function mostra_coisasquegosto(){
+    const item1 = document.getElementById
+    ("coisasgosto");
+    item1.style.display = "block";
+}
+
+function mostra_vida(){
+    const item1 = document.getElementById
+    ("vida");
+    item1.style.display = "block";
+}
+
+function mostra_planos(){
+    const item1 = document.getElementById
+    ("planos");
+    item1.style.display = "block";
+}
+
 function mostra_creditos(){
     const item1 = document.getElementById
     ("créditos");
     item1.style.display = "block";
 }
+
+
